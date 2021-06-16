Fixed Sobol Calculation for multiple QOIs

PythonPractice/UQtoolbox.py
@@ -339,12 +339,10 @@ def CalculateSobol(fA, fB, fAB, fD):
     else:
         for iQOI in range(0,nQOIs):
             #Calculate 1st order parameter effects
-            sobolBase[iQOI,:]=(np.sum(fAB[:,:,iQOI]*fA[:, [iQOI]],axis=0)-np.sum(fA[:,iQOI]*fB[:,iQOI],axis=0))/(nSamp*fDvar[iQOI])
+            sobolBase[iQOI,:]=np.sum(fB[:,[iQOI]]*(fAB[:,:,iQOI]-fA[:,[iQOI]]),axis=0)/(nSamp*fDvar[iQOI])
             #Caclulate 2nd order parameter effects
-            sobolTot[iQOI,:]=(np.sum(fA[:,iQOI]**2,axis=0)-2*np.sum(fA[:, [iQOI]]*fAB[:,:,iQOI],axis=0)\
-                                          +np.sum(fAB[:,:,iQOI]**2,axis=0))/(2*nSamp*fDvar[iQOI])
-    print(sobolBase.shape)
-    print(sobolTot.shape)
+            sobolTot[iQOI,:]= np.sum((fA[:,[iQOI]]-fAB[:,:,iQOI])**2,axis=0)/(2*nSamp*fDvar[iQOI])
+
 
 
     return sobolBase, sobolTot

PythonPractice/UQtoolbox_examples.py
@@ -87,12 +87,12 @@ def GetExample(example, **kwargs):
                        distParms=np.array([[-np.sqrt(12)/2, -3], [np.sqrt(12)/2, 3]]))
         options.gsa=uq.gsaOptions()
     elif example.lower() == 'aluminum rod (uniform)':
-        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([45,46])),
+        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([10, 65])),
                          basePOIs=np.array([-18.4, .00191]),
                          dist="uniform")
         options.gsa=uq.gsaOptions()
     elif example.lower() == 'aluminum rod (normal)':
-        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([45])),
+        model = uq.model(evalFcn=lambda params: HeatRod(params, np.array([10, 65])),
                          basePOIs=np.array([-18.4, .00191]),
                          dist="normal",
                          distParms=np.array([[-18.4, .00191], [.16*((-18.4)**2)/12, .16*(.00191**2)/12]]))